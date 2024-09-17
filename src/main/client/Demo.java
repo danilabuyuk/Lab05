6498432cecd678e112fe40f053112e02a2dba112
Daniel Buyuk <49594688+danilabuyuk@users.noreply.github.com>
2024-09-17 10:46:51 -0400

    Initial commit

---
+++ src/main/client/Demo.java
@@ -0,0 +1,23 @@
+package client;
+
+import apps.Calculator;
+
+/**
+ * Demo used to manually test the program.
+ * 
+ * @author X
+ * @version Y
+ */
+public class Demo
+{
+	/**
+     * Runs the demo.
+     * 
+     * @param args command line arguments
+     */
+    @SuppressWarnings("unused")
+    public static void main(String[] args) 
+    {
+        Calculator c = new Calculator();
+    }
+}
